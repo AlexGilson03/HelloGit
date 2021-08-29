3042734c1ed3be2b9842be7e0c6cbee2bf07048c
cadenchavez <cadenchavez@localhost>
2021-08-29 17:22:04 -0500

    Added partner name and major

--- src/Hello.java
+++ src/Hello.java
@@ -2,9 +2,12 @@
 public class Hello{
 	
 	public static void main(String[] args) {
+		
 		System.out.println("Alex Gilson");
 		System.out.println("Computer Science");
-		
+		System.out.println("Caden Chavez");
+		System.out.println("Computer Science");
+
 		
 		
 	}
